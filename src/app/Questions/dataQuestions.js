54522c27554e955290a8e2478dcbb72c479ed82e
R. LaRoi <are@raewhoo.com>
2024-06-03 21:59:21 -0400

    Create dataQuestions.js

---
+++ src/app/Questions/dataQuestions.js
@@ -0,0 +1,44 @@
+export const selfAssesmentQuestions = [
+  {
+    id: 1,
+    question:
+      "Where are you in your career? Are you just starting out, experienced, or an expert?",
+    selections: ["early", "experienced", "expert"],
+  },
+
+  { id: 2, question: "List your strengths" },
+
+  { id: 3, question: "What are you looking for in your next career?" },
+  {
+    id: 4,
+    question: "What have you valued about your previous work experience?",
+  },
+  { id: 5, question: "List your concerns about your career." },
+
+  { id: 6, question: "What is your preferred industry." },
+  { id: 7, question: "what would be your dream accomplishment?" },
+
+  {
+    id: 8,
+    question: "what motivates you to pursue a new career?",
+  },
+
+  {
+    id: 9,
+
+    question: "List what factors matter most to you in your next role?",
+
+    selections: [
+      "There are many job openings",
+      "The hours are predictable",
+      "The wages are good",
+      "The healthcare and benefits are good",
+      "Its a great fit with my interests and skillful",
+      "I have always dreamed of a career like this ",
+      " There are opportunities for advancement",
+      "I have a passion for this industry",
+      " Other: ",
+      "I have the natural abilities to be success",
+    ],
+  },
+];
