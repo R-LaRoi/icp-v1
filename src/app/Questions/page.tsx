6dcaf0ff56c5911f1dae472808ceb443a1894606
R. LaRoi <are@raewhoo.com>
2024-06-05 18:46:01 -0400

    Questions Component
    Added Nav, Button, Image

--- src/app/Questions/page.tsx
+++ src/app/Questions/page.tsx
@@ -1,12 +1,37 @@
 'useClient'
 import React from "react";
+import { Navigation } from "../icpHome/nav";
+import Image from 'next/image'
+import sandy from './sandy.jpg'
 
 export default function Questions () {
 
 
   return(
- <section>
-  <h1 className="text-red-400">career planning</h1>
+<section>
+ <Navigation />
+ <section className="p-12">
+ <div className="questions-img">
+  {/* {mainImg} */}
+  <Image src={sandy}
+        width={400}
+        height={400}
+        alt="illustration"
+        priority={true} />
+</div>
+
+<main>
+  <div className="form m-16 ">
+ <h1 className = "font-light text-7xl text-stone-600">One step at a time.</h1>
+ <p className='text-2xl mt-5 p-2'>Let's get started with a few questions about you.</p>
+ <button className="arrows mx-2 mt-2 px-2"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-12">
+  <path strokeLinecap="round" strokeLinejoin="round" d="m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5" />
+</svg>
+
+</button>
+ </div>
+</main>
+  </section>
   </section>
   )
  
