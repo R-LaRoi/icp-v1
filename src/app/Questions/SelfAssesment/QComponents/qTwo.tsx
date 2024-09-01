ec5bf1e3ae1b4d8a57f6af037d84c9fccac5ff6f
R. LaRoi <are@raewhoo.com>
2024-09-01 16:28:51 -0400

    form.tsx

--- src/app/Questions/SelfAssesment/QComponents/qTwo.tsx
+++ src/app/Questions/SelfAssesment/QComponents/qTwo.tsx
@@ -1,50 +1,48 @@
 import { QWrapper } from '../../QWrapper'
-import {selfAssesmentQuestions} from '../dataQuestions'
+import { selfAssesmentQuestions } from '../dataQuestions'
 
 
 let selfQ = selfAssesmentQuestions
 
 
-
-
 type UserTextData = {
-  strengths : string
+  strengths: string
 }
 
 type QTwoProps = UserTextData & {
-  updateTextArea: (textArea: Partial<UserTextData> ) => void
+  updateTextArea: (textArea: Partial<UserTextData>) => void
 }
 
 
-export function QTwo({strengths, updateTextArea }:QTwoProps){
- let introQ2 = selfQ.find((selfOne) => selfOne.id === 2)
-  return(
+export function QTwo({ strengths, updateTextArea }: QTwoProps) {
+  let introQ2 = selfQ.find((selfOne) => selfOne.id === 2)
+  return (
 
-  <QWrapper title="Introspective">
-<div key={introQ2?.id}>
-<div className='font-regular m-4 text-3xl text-stone-600'>
+    <QWrapper title="Introspective">
+      <div key={introQ2?.id}>
+        <div className='font-regular m-4 text-3xl text-stone-600'>
 
-{introQ2?.question}
-</div>
-<div className='font-light m-4 text-2xl text-stone-600'>
-{introQ2?.selections}
-</div>
-<div className="relative w-full min-w-[200px]">
-    <textarea
-    name="strengths"
-    onChange={e => updateTextArea({strengths: e.target.value})}
-    value = {strengths}
-      className="peer h-full min-h-[100px] w-75 resize-none border-b border-blue-gray-200 bg-transparent pt-4 pb-1.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border-blue-gray-200 focus:border-gray-900 focus:outline-0 disabled:resize-none disabled:border-0 disabled:bg-blue-gray-50"
-      placeholder=" "
-      required>
-    </textarea>
-    <label
-      className="after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-500 transition-all after:absolute after:-bottom-0 after:block after:w-full after:scale-x-0 after:border-b-2 after:border-gray-900 after:transition-transform after:duration-300 peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.25] peer-placeholder-shown:text-blue-gray-500 peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:after:scale-x-100 peer-focus:after:border-gray-900 peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
-Strengths
-    </label>
-  </div>
-</div>
-</QWrapper>
+          {introQ2?.question}
+        </div>
+        <div className='font-light m-4 text-2xl text-stone-600'>
+          {introQ2?.selections}
+        </div>
+        <div className="relative w-full min-w-[200px]">
+          <textarea
+            name="strengths"
+            onChange={e => updateTextArea({ strengths: e.target.value })}
+            value={strengths}
+            className="peer h-full min-h-[100px] w-75 resize-none border-b border-blue-gray-200 bg-transparent pt-4 pb-1.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border-blue-gray-200 focus:border-gray-900 focus:outline-0 disabled:resize-none disabled:border-0 disabled:bg-blue-gray-50"
+            placeholder=" "
+            required>
+          </textarea>
+          <label
+            className="after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-500 transition-all after:absolute after:-bottom-0 after:block after:w-full after:scale-x-0 after:border-b-2 after:border-gray-900 after:transition-transform after:duration-300 peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.25] peer-placeholder-shown:text-blue-gray-500 peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:after:scale-x-100 peer-focus:after:border-gray-900 peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+            Strengths
+          </label>
+        </div>
+      </div>
+    </QWrapper>
 
 
   )
--- src/app/icpHome/form.tsx
+++ src/app/icpHome/form.tsx
@@ -1,83 +1,79 @@
 'use client'
 import './icpHome.css'
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { useRouter } from "next/navigation";
 
 
 export const MainForm = (): React.ReactNode => {
-let [userInput, setUserInput] = useState({});
+  let [userInput, setUserInput] = useState({});
   const router = useRouter()
 
- function formSubmitted (event:any)  {
-        event.preventDefault();
+  function formSubmitted(event: any) {
+    event.preventDefault();
 
-        const submitData = {
-          ...userInput
-        }
-        router.push('/Questions')
-        console.log(submitData)
-}
+    const submitData = {
+      ...userInput
+    }
+    router.push('/Questions')
+    console.log(submitData)
+  }
 
-function formChange (event:any) {
-       event.preventDefault();
-   const { target } = event;
+  function formChange(event: any) {
+    event.preventDefault();
+    const { target } = event;
     const { name, value } = target;
 
-      setUserInput({
+    setUserInput({
       ...userInput, // Keep existing form data
       [name]: value // Update form data for the input field that changed
     });
-}
+  }
 
 
 
-return(
+  return (
 
- <div className="form m-16 ">
- <h1 className = "font-light text-7xl h1-form">Get closer to your goal today.</h1>
- <p className='text-2xl mt-5 p-2'>Let us help you plan ahead and take the
- next step <br/> towards your new career.</p>
- <form onSubmit = {formSubmitted}
+    <div className="form m-16 ">
+      <h1 className="font-light text-7xl h1-form">Get closer to your goal today.</h1>
+      <p className='text-2xl mt-5 p-2'>Let us help you plan ahead and take the
+        next step <br /> towards your new career.</p>
+      <form onSubmit={formSubmitted}
 
-  className="h-96 bg-white rounded pb-5 mb-4">
-    
-    <div className=" pt-10 mb-4">
-      {/* <label className="block text-gray-700 text-sm font-bold mb-2" >
- Name
-      </label> */}
-      <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
-      id="username" 
-      min={7}
-      type="text" 
-      placeholder="enter name"
-      onChange={formChange} 
-      required
-      name="userInput"
+        className="h-96 bg-white rounded pb-5 mb-4">
 
-      />
+        <div className=" pt-10 mb-4">
+
+          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="username"
+            min={7}
+            type="text"
+            placeholder="enter name"
+            onChange={formChange}
+            required
+            name="userInput"
+
+          />
+        </div>
+        <div className="mb-6">
+
+          <input className="shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="email"
+            type="email"
+            name="email"
+            placeholder="enter email"
+            onChange={formChange}
+            required
+          />
+
+        </div>
+        <div className="flex items-center justify-between">
+          <button className="start-btn bg-hover:stone-400 text-white font-bold py-2 px-8 focus:outline-none focus:shadow-outline" type="submit"
+          >
+            GET STARTED
+          </button>
+
+        </div>
+      </form>
     </div>
-    <div className="mb-6">
-      {/* <label className="block text-gray-700 text-sm font-bold mb-2" >
-    Email
-      </label> */}
-      <input className="shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="email" 
-      type="email" 
-      name = "email"
-      placeholder="enter email"
-       onChange={formChange}
-         required
-              />
-    
-    </div>
-    <div className="flex items-center justify-between">
-      <button className="start-btn bg-hover:stone-400 text-white font-bold py-2 px-8 focus:outline-none focus:shadow-outline" type="submit" 
-    >
-GET STARTED
-      </button>
-   
-    </div>
-  </form>
- </div>
 
 
   )
