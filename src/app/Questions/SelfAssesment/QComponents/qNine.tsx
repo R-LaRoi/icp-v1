5239c509ee2a2ab2e4c9b43e2045eafc0ece3907
R. LaRoi <are@raewhoo.com>
2024-09-02 14:59:29 -0400

    Tailwind CSS
    Form adjustments

--- src/app/Questions/SelfAssesment/QComponents/qNine.tsx
+++ src/app/Questions/SelfAssesment/QComponents/qNine.tsx
@@ -1,35 +1,42 @@
 import { QWrapper } from '../../QWrapper'
-import {selfAssesmentQuestions} from '../dataQuestions'
+import { selfAssesmentQuestions } from '../dataQuestions'
 
 let selfQ = selfAssesmentQuestions
 
-export function QNine(){
-let introQ9 = selfQ.find((selfOne) => selfOne.id === 9)
-const qOptions = introQ9?.selections;
+export function QNine() {
+  let introQ9 = selfQ.find((selfOne) => selfOne.id === 9)
+  const qOptions = introQ9?.selections;
 
 
 
-  return(
-<QWrapper title="Introspective">
- 
-<div key={introQ9
-?.id}>
-<div className='font-regular m-4 text-3xl text-stone-600'>
+  return (
+    <QWrapper title="Introspective">
 
-{introQ9?.question}
-</div>
-<div className='font-light m-4 text-2xl text-stone-600'>
-<ul>
-   {qOptions?.map((item,)=> {
-  return (<li key={item}>{item}</li> 
+      <div key={introQ9
+        ?.id}>
+        <div className='font-regular m-4 text-3xl text-stone-600'>
+
+          {introQ9?.question}
+        </div>
+        <div className='font-light m-4 text-2xl text-stone-600'>
+          <ul>
+            {qOptions?.map((item,) => {
+              return (
+                <li key={item}>
+                  <label className="flex items-center space-x-2">
+                    <input type="checkbox" className="form-checkbox h-5 w-5 text-stone-600 rounded border-stone-600 focus:ring-stone-600 mr-2">
+                    </input>
+                    {item}
+                  </label>
+                </li>
+              )
+
+            })}
+          </ul>
+        </div>
+      </div>
+    </QWrapper>
+
+
   )
-
- })} 
-</ul>
-</div>
-</div>
-</QWrapper>
-
-
-  )
-  }
+}
--- src/app/Questions/SelfAssesment/QComponents/qOne.tsx
+++ src/app/Questions/SelfAssesment/QComponents/qOne.tsx
@@ -1,35 +1,35 @@
 import { QWrapper } from '../../QWrapper'
-import {selfAssesmentQuestions} from '../dataQuestions'
+import { selfAssesmentQuestions } from '../dataQuestions'
 
 let selfQ = selfAssesmentQuestions
 
-export function QOne(){
-let introQ = selfQ.find((selfOne) => selfOne.id === 1)
-const qOptions = introQ?.selections;
+export function QOne() {
+  let introQ = selfQ.find((selfOne) => selfOne.id === 1)
+  const qOptions = introQ?.selections;
 
 
 
-  return(
-<QWrapper title="Introspective">
- 
-<div key={introQ
-?.id}>
-<div className='font-regular m-4 text-3xl text-stone-600'>
+  return (
+    <QWrapper title="Introspective">
 
-{introQ?.question}
-</div>
-<div className='font-light m-4 text-2xl text-stone-600'>
-<ul>
-   {qOptions?.map((item,)=> {
-  return (<li key={item}>{item}</li> 
+      <div key={introQ
+        ?.id}>
+        <div className='font-regular m-4 text-3xl text-stone-600'>
+
+          {introQ?.question}
+        </div>
+        <div className='font-light m-4 text-1xl text-stone-600 '>
+          <ul className='flex space-x-4'>
+            {qOptions?.map((item,) => {
+              return (<li className=' inline-block border border-stone-600 p-2 rounded-lg hover:bg-stone-600 hover:text-white' key={item}>{item}</li>
+              )
+
+            })}
+          </ul>
+        </div>
+      </div>
+    </QWrapper>
+
+
   )
-
- })} 
-</ul>
-</div>
-</div>
-</QWrapper>
-
-
-  )
-  }
+}
