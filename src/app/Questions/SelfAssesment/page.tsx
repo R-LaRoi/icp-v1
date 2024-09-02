08c7a4761730c8c64d6f0afffa373581ef1b72cd
R. LaRoi <are@raewhoo.com>
2024-09-02 17:15:04 -0400

    Update page.tsx

--- src/app/Questions/SelfAssesment/page.tsx
+++ src/app/Questions/SelfAssesment/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { FormEvent , useState } from "react";
+import React, { FormEvent, useState } from "react";
 import { SelfAQForm } from "./selfAQForm";
 import { QOne } from "./QComponents/qOne";
 import { QTwo } from "./QComponents/qTwo";
@@ -14,89 +14,90 @@ import { Navigation } from "../../icpHome/nav"
 
 
 type FormData = {
-strengths:string
-next_career: string 
-wkExp_values: string
-career_concerns: string
-industry_pref: string
-dream_acc: string
-motivation: string
+  strengths: string
+  next_career: string
+  wkExp_values: string
+  career_concerns: string
+  industry_pref: string
+  dream_acc: string
+  motivation: string
 }
 
 const INIIAL_DATA: FormData = {
-strengths:'',
-next_career: '',
-wkExp_values: '',
-career_concerns: '',
-industry_pref: '',
-dream_acc: '',
-motivation: '',
+  strengths: '',
+  next_career: '',
+  wkExp_values: '',
+  career_concerns: '',
+  industry_pref: '',
+  dream_acc: '',
+  motivation: '',
 }
 
 
-export default function SelfAssesmentQuestions () {
-const [data, setData] = useState(INIIAL_DATA)
+export default function SelfAssesmentQuestions() {
+  const [data, setData] = useState(INIIAL_DATA)
 
-function updateTextArea(textArea: Partial<FormData>){
+  function updateTextArea(textArea: Partial<FormData>) {
 
-  setData(prev => {
+    setData(prev => {
 
-    return {...prev, ...textArea}
-  })
-}
-
-
-const {questions, questionIndex, question, backQtn, nextQtn } = SelfAQForm([ <QOne key="1" {...data}  />,
-<QTwo key="2" {...data}  updateTextArea={updateTextArea}/>,
-<QThree key="3" {...data}  updateTextArea={updateTextArea}/>,
-<QFour key="4" {...data}  updateTextArea={updateTextArea}/>,<QFive key="5" {...data}  updateTextArea={updateTextArea}/>,
-<QSix key="5" {...data}  updateTextArea={updateTextArea}/>,
-<QSix key="6" {...data}  updateTextArea={updateTextArea}/>,
-<QSeven key="7" {...data}  updateTextArea={updateTextArea}/>,
-<QEight key="8" {...data}  updateTextArea={updateTextArea}/>,
-<QNine key="9" {...data} /> 
-])
+      return { ...prev, ...textArea }
+    })
+  }
 
 
-
-function submitForm(e: FormEvent){
-  e.preventDefault();
-
- if (questionIndex !== questions.length - 1){
-  nextQtn();
- } else {
-  //  send the data
-  alert('form submitted')
-  console.log(data)
- }
-
-}
-
-return(
+  const { questions, questionIndex, question, backQtn, nextQtn } = SelfAQForm([<QOne key="1" {...data} />,
+  <QTwo key="2" {...data} updateTextArea={updateTextArea} />,
+  <QThree key="3" {...data} updateTextArea={updateTextArea} />,
+  <QFour key="4" {...data} updateTextArea={updateTextArea} />, <QFive key="5" {...data} updateTextArea={updateTextArea} />,
+  <QSix key="5" {...data} updateTextArea={updateTextArea} />,
+  <QSix key="6" {...data} updateTextArea={updateTextArea} />,
+  <QSeven key="7" {...data} updateTextArea={updateTextArea} />,
+  <QEight key="8" {...data} updateTextArea={updateTextArea} />,
+  <QNine key="9" {...data} />
+  ])
 
 
-<div>
-   <Navigation />
-<form onSubmit={submitForm}>
-<div>
-{questionIndex + 1} \ {questions.length}
-</div>
-{question}
-<div style={{ marginTop: "1rem", display:"flex", justifyContent: 'flex-end', gap:'.5rem'}}>
-  {questionIndex !== 0 &&  
-  <button type="button"  
-  onClick={backQtn}>Back</button> }
 
-  <button 
-  className="arrows mx-2 mt-2 px-2"
+  function submitForm(e: FormEvent) {
+    e.preventDefault();
 
-  type="submit" >
-    {questionIndex === questions.length - 1 ? "done" : "next" }</button>
-</div>
-</form>
-</div>
+    if (questionIndex !== questions.length - 1) {
+      nextQtn();
+    } else {
+      //  send the data
+      alert('form submitted')
+      console.log(data)
+    }
 
-)
+  }
+
+  return (
+
+
+    <div>
+      <Navigation />
+      <form onSubmit={submitForm}>
+        <div>
+          {questionIndex + 1} \ {questions.length}
+        </div>
+        {question}
+        <div className="flex justify-end gap-2 mt-4 mb-8">
+          {questionIndex !== 0 &&
+            <button className="border text-stone-600 hover:bg-orange-300 hover:text-white font-bold py-2 px-8 rounded focus:outline-none focus:shadow-outline transition-colors duration-200"
+              type="button"
+              onClick={backQtn}>BACK</button>}
+
+          <button
+            className="bg-stone-600 hover:bg-orange-300 text-white font-bold py-2 px-8 rounded focus:outline-none focus:shadow-outline transition-colors duration-200"
+
+            type="submit" >
+            {questionIndex === questions.length - 1 ? "DONE" : "NEXT"}</button>
+        </div>
+      </form>
+    </div>
+
+  )
 
 
 }
