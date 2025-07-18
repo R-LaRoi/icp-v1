a6f2eb170a9640486b2b0e9d46202cd9ea30cf6f
R. LaRoi <are@raewhoo.com>
2025-07-18 15:53:38 -0400

    removed prisma

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,11 @@
 
 import NextAuth from "next-auth";
 import EmailProvider from "next-auth/providers/email";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient(); 
+
 
 const handler = NextAuth({
-  adapter: PrismaAdapter(prisma),
+
   providers: [
     EmailProvider({
       server: {
